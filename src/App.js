5952db415c6ff10f47831f032e03684f6371eaef
Alen245 <akiyanovski24@gmail.com>
2023-06-23 15:24:04 -0400

    4.10

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import CitySearch from './CitySearch';
 import { getEvents, extractLocations } from './api';
 import './nprogress.css';
 import NumberOfEvents from "./NumberOfEvents";
+import {
+  ScatterChart, Scatter, XAxis, YAxis, CartesianGrid, Tooltip
+} from 'recharts';
 
 
 
@@ -38,15 +41,42 @@ class App extends Component {
     });
   };
 
+  getData = () => {
+    const { locations, events } = this.state;
+    const data = locations.map((location) => {
+      const number = events.filter((event) => event.location === location).length
+      const city = location.split(', ').shift()
+      return { city, number };
+    })
+    return data;
+  };
+
   render() {
     return (
       <div className="App">
+
         <CitySearch locations={this.state.locations} updateEvents={this.updateEvents} />
         <EventList events={this.state.events} />
         <NumberOfEvents
           numberOfEvents={this.state.numberOfEvents}
           updateEvents={this.updateEvents}
         />
+        <ScatterChart
+          width={800}
+          height={250}
+          margin={{
+            top: 20,
+            right: 20,
+            bottom: 10,
+            left: 10,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          <XAxis type="category" dataKey="city" name="city" />
+          <YAxis type="number" dataKey="number" name="number of events" />
+          <Tooltip cursor={{ strokeDasharray: '3 3' }} />
+          <Scatter data={this.getData()} fill="#8884d8" />
+        </ScatterChart>
       </div>
     );
   }
