0ce6b266b0b0d6d6f2ad9b6fde397812f9fdf275
Alen245 <akiyanovski24@gmail.com>
2023-06-28 15:13:32 -0400

    4.10 fixed 1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
-import React, { Component } from 'react';
-import './App.css';
-import EventList from './EventList';
-import CitySearch from './CitySearch';
-import { getEvents, extractLocations } from './api';
-import './nprogress.css';
-import EventGenre from './EventGenre';
-import NumberOfEvents from './NumberOfEvents';
+import React, { Component } from "react";
+import "./App.css";
+import EventList from "./EventList";
+import CitySearch from "./CitySearch";
+import { getEvents, extractLocations } from "./api";
+import "./nprogress.css";
+import EventGenre from "./EventGenre";
+import NumberOfEvents from "./NumberOfEvents";
+import WelcomeScreen from "./WelcomeScreen";
 
 class App extends Component {
   state = {
@@ -13,8 +14,8 @@ class App extends Component {
     locations: [],
     eventCount: 32,
     selectedCity: null,
-    warningText: '',
-    showWelcomeScreen: undefined,
+    warningText: "",
+    isLoggedIn: false,
   };
 
   componentDidMount() {
@@ -30,18 +31,31 @@ class App extends Component {
     });
   };
 
+  handleLogin = () => {
+    // Perform login logic here, e.g., authenticate with Google
+
+    // If login is successful, update the state
+    this.setState({ isLoggedIn: true });
+  };
+
   render() {
-    const { events, locations } = this.state;
+    const { events, locations, isLoggedIn } = this.state;
 
     return (
       <div className="App">
-        <CitySearch locations={locations} updateEvents={this.updateEvents} />
-        <EventGenre events={events} />
-        <EventList events={events} />
-        <NumberOfEvents
-          numberOfEvents={this.state.numberOfEvents}
-          updateEvents={this.updateEvents}
-        />
+        {!isLoggedIn ? (
+          <WelcomeScreen handleLogin={this.handleLogin} />
+        ) : (
+          <>
+            <CitySearch locations={locations} updateEvents={this.updateEvents} />
+            <EventGenre events={events} />
+            <EventList events={events} />
+            <NumberOfEvents
+              numberOfEvents={this.state.numberOfEvents}
+              updateEvents={this.updateEvents}
+            />
+          </>
+        )}
       </div>
     );
   }
